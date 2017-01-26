dc651d3d94c86db7e1fdec038017def1cf9b80b8
Gurjot536 <gurjot536@gmail.com>
2017-01-25 19:51:42 -0800

    api

---
+++ sketch.js
@@ -0,0 +1,55 @@
+var x;
+var web = 'http://api.openweathermap.org/data/2.5/weather?q=';
+var apikey = '&appid=49ff3214f9e25759de77a7afe42469ca&units=metric';
+var url;
+var weather;
+var fire;
+var ice;
+var thumbs;
+
+ function preload() {
+   fire = loadImage('images/fire.png');
+   ice = loadImage('images/ice.png');
+   thumbs = loadImage('images/thumbs.png')
+ }
+ 
+function setup() {
+  createCanvas(600,400);
+  x = select('#city');
+   var button = select('#enter');
+   button.mousePressed(ask);
+
+}
+
+ function gotData(data) {
+  weather = data;
+ }
+
+function draw() {
+  background(0);
+  url = web + x.value() + apikey;
+  fill(random(255),random(255),random(255));
+  imageMode(CENTER);
+  if (weather) {
+    var temp = weather.main.temp;
+    textAlign(CENTER);
+    textSize(25);
+      text('Temperature: '+temp+'°C',120,35);
+  }
+  if(temp > 30) {
+    image(fire,width/2,200,200,200);
+  }
+  
+  if(temp < 0 && temp > -100) {
+    image(ice,width/2,200,200,200);
+  }
+  
+  if(temp > 0 && temp < 30) {
+    image(thumbs,width/2,200,200,200);
+  } 
+}
+
+function ask() {
+  loadJSON(url,gotData);
+  
+}
