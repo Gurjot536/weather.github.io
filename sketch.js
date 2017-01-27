dbc1ed13dadbef2b3881a748c786607067c5daf0
Gurjot536 <gurjot536@gmail.com>
2017-01-26 18:02:27 -0800

    rip #2

--- sketch.js
+++ sketch.js
@@ -32,11 +32,9 @@ function draw() {
   imageMode(CENTER);
   if (weather) {
     var temp = weather.main.temp;
-    var des = weather.main.value();
     textAlign(CENTER);
     textSize(25);
       text('Temperature: '+temp+'°C',120,35);
-      text('Description: '+des,120,65);
   }
   if(temp > 30) {
     image(fire,width/2,200,200,200);
